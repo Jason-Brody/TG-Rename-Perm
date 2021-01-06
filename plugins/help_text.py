e7ab1806d18356e966fe202e86f53712e75ecd6a
Jason-Brody <65779952+Jason-Brody@users.noreply.github.com>
2021-01-06 21:57:40 +0530

    Done Something

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -50,12 +50,6 @@ def send_start(bot, update):
         reply_to_message_id=update.message_id
     )
 
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-
-
 @Client.on_message(filters.command(["upgrade"]))
 def upgrade(bot, update):
     # logger.info(update)
